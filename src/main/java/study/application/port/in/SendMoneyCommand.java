cf37968c6773e9635015f699542d0ca327621a11
jeonghuntech <jeonghun.tech@gmail.com>
2024-02-05 18:16:44 +0900

    feat: 입력 모델 추가

---
+++ src/main/java/study/application/port/in/SendMoneyCommand.java
@@ -0,0 +1,34 @@
+package study.application.port.in;
+
+import static study.domain.Account.*;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.EqualsAndHashCode;
+import lombok.Value;
+import study.common.SelfValidating;
+import study.domain.Money;
+
+@Value
+@EqualsAndHashCode(callSuper = false)
+public
+class SendMoneyCommand extends SelfValidating<SendMoneyCommand> {
+
+    @NotNull
+    private final AccountId sourceAccountId;
+
+    @NotNull
+    private final AccountId targetAccountId;
+
+    @NotNull
+    private final Money money;
+
+    public SendMoneyCommand(
+            AccountId sourceAccountId,
+            AccountId targetAccountId,
+            Money money) {
+        this.sourceAccountId = sourceAccountId;
+        this.targetAccountId = targetAccountId;
+        this.money = money;
+        this.validateSelf();
+    }
+}
